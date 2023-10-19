c55d8a0ec7972856ba68aee8c4b80265b793719f
Alexx231 <147101685+Alexx231@users.noreply.github.com>
2023-10-19 07:57:01 +0000

    xd

--- shdgd/numeropar.py
+++
@@ -1,5 +0,0 @@
-def numero_par1():
-    numero_par = [2,4,6,8,10]
-    if numero_par % 2 == 0: 
-     print(numero_par, "es par.")
-numero_par1()
--- shdgd/pedro.py
+++
@@ -1,8 +0,0 @@
-libros = []
-for i in range (10) :
-    libro = str(input("Dime un libro : "))
-    libros.append(libro)
-    
-    for libro in libros : 
-        print(libro)
-    
